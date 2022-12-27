d750959b95181578e2d0f9a69f25fcc116542301
MingFEW <mingfework@gmail.com>
2022-12-27 23:40:15 +0700

    fix: using unix instead of Date object

--- src/models/machine.ts
+++ src/models/machine.ts
@@ -3,6 +3,6 @@ import { MachineField, MachineFieldValue } from './machine-field'
 export type Machine<F extends MachineField[] = MachineField[]> = {
   id: string
   categoryId: string
-  createdAt: Date
+  createdAt: number
   data: MachineFieldValue<F[number]>[]
 }
