787674ff6fdcba2fe8dab89143283e7b7e6e4fe4
MingFEW <mingfework@gmail.com>
2022-12-27 20:42:38 +0700

    fix: missing type configuration

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,4 @@
+export { default as themes } from './themes'
 export { default as Common } from './Common'
 export { default as Fonts } from './Fonts'
 export { default as Gutters } from './Gutters'
